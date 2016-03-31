132815654da5fd3ac18e25e6803e5da6f49f9c73
Netguru - Michał Sajnóg <michal.sajnog@netguru.co>
2016-03-31 12:58:58 +0200

    Initial plugin structure

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/* eslint-disable global-require */
+
+var fs = require('fs');
+var path = require('path');
+var environments = require('./environments');
+
+var rules = {};
+var ruleDir = path.join(__dirname, 'rules');
+
+fs.readdirSync(ruleDir).forEach(function(name) {
+  var match = name.match(/(.+)\.js$/);
+  if (match) {
+    rules[match[1]] = require(path.join(ruleDir, name));
+  }
+});
+
+module.exports = {
+  rules: rules,
+  environments: environments
+};
---
+++ rules/simple-rule.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+module.exports = function(context) {
+
+  return {
+    // properties go here
+  };
+
+};
+
+module.exports.schema = [
+  // JSON Schema for rule options goes here
+];
