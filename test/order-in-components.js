753f1639ed412b42c79c6934bdaaf6d696c9fcfe
Rafal Leszczynski <raf.leszcz@gmail.com>
2016-08-28 18:21:22 +0200

    extend test suite for order in components rule

--- test/order-in-components.js
+++ test/order-in-components.js
@@ -25,7 +25,27 @@ eslintTester.run('order-in-components', rule, {
     {
       code: 'export default Component.extend({levelOfHappiness: computed("attitude", "health", () => {\n}), actions: {}});',
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
-    }
+    },
+    {
+      code: 'export default Component.extend({role: "sloth", abc: Ember.inject.service(), def: inject.service(), ghi: service(), levelOfHappiness: computed("attitude", "health", () => {\n})});',
+      parserOptions: {ecmaVersion: 6, sourceType: "module"},
+    },
+    {
+      code: 'export default Component.extend({role: "sloth", abc: [], def: {}, ghi: service()});',
+      parserOptions: {ecmaVersion: 6, sourceType: "module"},
+    },
+    {
+      code: 'export default Component.extend({levelOfHappiness: computed("attitude", "health", () => {\n}), abc: Ember.observer("aaaa", function () {\n}), def: observer("aaaa", function () {\n}), actions: {}});',
+      parserOptions: {ecmaVersion: 6, sourceType: "module"},
+    },
+    {
+      code: 'export default Component.extend({abc: observer("aaaa", () => {\n}), init: function () {\n}, actions: {}, customFunction() {\n}});',
+      parserOptions: {ecmaVersion: 6, sourceType: "module"},
+    },
+    {
+      code: 'export default Component.extend({abc: [], def: true, igh: service(), singleComp: alias("abc"), multiComp: computed(() => {\n}), obs: observer("aaa", function () {\n}), init: function () {\n}, actions: {}, customFunc: function() {\n}});',
+      parserOptions: {ecmaVersion: 6, sourceType: "module"},
+    },
   ],
   invalid: [
     {
@@ -44,6 +64,55 @@ eslintTester.run('order-in-components', rule, {
     },
     {
       code: 'export default Component.extend({levelOfHappiness: computed("attitude", "health", () => {\n}), vehicle: alias("car"), role: "sloth", actions: {}});',
+      parserOptions: {ecmaVersion: 6, sourceType: "module"},
+      errors: [{
+        message: 'Check order of properties',
+      }],
+    },
+    {
+      code: 'export default Component.extend({i18n: service(), abc: true});',
+      parserOptions: {ecmaVersion: 6, sourceType: "module"},
+      errors: [{
+        message: 'Check order of properties',
+      }],
+    },
+    {
+      code: 'export default Component.extend({vehicle: alias("car"), i18n: service()});',
+      parserOptions: {ecmaVersion: 6, sourceType: "module"},
+      errors: [{
+        message: 'Check order of properties',
+      }],
+    },
+    {
+      code: 'export default Component.extend({levelOfHappiness: observer("attitude", "health", () => {\n}), vehicle: alias("car")});',
+      parserOptions: {ecmaVersion: 6, sourceType: "module"},
+      errors: [{
+        message: 'Check order of properties',
+      }],
+    },
+    {
+      code: 'export default Component.extend({levelOfHappiness: observer("attitude", "health", () => {\n}), aaa: computed("attitude", "health", () => {\n})});',
+      parserOptions: {ecmaVersion: 6, sourceType: "module"},
+      errors: [{
+        message: 'Check order of properties',
+      }],
+    },
+    {
+      code: 'export default Component.extend({init() {\n}, levelOfHappiness: observer("attitude", "health", () => {\n})});',
+      parserOptions: {ecmaVersion: 6, sourceType: "module"},
+      errors: [{
+        message: 'Check order of properties',
+      }],
+    },
+    {
+      code: 'export default Component.extend({actions: {}, init() {\n}});',
+      parserOptions: {ecmaVersion: 6, sourceType: "module"},
+      errors: [{
+        message: 'Check order of properties',
+      }],
+    },
+    {
+      code: 'export default Component.extend({customFunc() {\n}, actions: {}});',
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
       errors: [{
         message: 'Check order of properties',
