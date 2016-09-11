1b9eaa329b8591aefcd0c99929f2e467c05ce30f
Rafal Leszczynski <raf.leszcz@gmail.com>
2016-09-11 12:47:32 +0200

    add failing test cases for multiple segments

--- test/routes-segments-snake-case.js
+++ test/routes-segments-snake-case.js
@@ -20,7 +20,13 @@ eslintTester.run('routes-segments-snake-case', rule, {
     'this.route("facebook-messages", { path: "fb-messages" });',
     'this.route("summary", { path: "/" });',
     'this.route("reset-password", function() {this.route("update");});',
-    'this.test()'
+    'this.test()',
+    'this.route("tree", { path: ":tree_id/:tree_child_id"});',
+    'this.route("tree", { path: "/test/:tree_id/:tree_child_id"});',
+    'this.route("tree", { path: "/test/:tree_id/test/:tree_child_id"});',
+    'this.route("tree", { path: "/test/:tree_id/test_test/:tree_child_id"});',
+    'this.route("tree", { path: "/test/:tree_id/test-test/:tree_child_id"});',
+    'this.route("tree", { path: "/test/:tree_id/testTest/:tree_child_id"});',
   ],
   invalid: [
     {
@@ -39,6 +45,20 @@ eslintTester.run('routes-segments-snake-case', rule, {
     },
     {
       code: 'this.route("tree", { path: "/test/:treeId"});',
+      parserOptions: {ecmaVersion: 6, sourceType: "module"},
+      errors: [{
+        message: 'Use snake case in dynamic segments of routes',
+      }],
+    },
+    {
+      code: 'this.route("tree", { path: "/test/treeId/:treeChildId"});',
+      parserOptions: {ecmaVersion: 6, sourceType: "module"},
+      errors: [{
+        message: 'Use snake case in dynamic segments of routes',
+      }],
+    },
+    {
+      code: 'this.route("tree", { path: "/test/tree-id/:tree-child-id"});',
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
       errors: [{
         message: 'Use snake case in dynamic segments of routes',
