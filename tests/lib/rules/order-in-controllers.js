9014cc36fdf193975f9cdc07f8d09537ed2870b9
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-02-05 19:51:29 +0100

    Report correct positions for "order-in-*" rules (#15)
    * tests/rules/order-in-components: Use template strings for "code" properties
    
    * rules/order-in-components: Extract ComponentPropertyInfo class
    
    * rules/order-in-components: Implement "type" property
    
    and derive "order" from that
    
    * rules/order-in-components: Inline mappedProperties variable
    
    * tests/rules/order-in-components: Assert "line" properties
    
    * rules/order-in-components: Report multiple properties
    
    ... that need to be moved further up
    
    * rules/order-in-components: Report correct positions
    
    * rules/order-in-components: Add "name" property
    
    * rules/order-in-components: Merge the two loops
    
    * rules/order-in-components: Remove unused variable and function
    
    * rules/order-in-components: Convert "type" to local variable
    
    * rules/order-in-components: Extract reusable function
    
    * rules/order-in-components: Move export to the bottom
    
    * tests/rules/order-in-controllers: Use template strings for "code" properties
    
    * tests/rules/order-in-models: Use template strings for "code" properties
    
    * tests/rules/order-in-routes: Use template strings for "code" properties
    
    * rules/order-in-controllers: Include correct position in warning
    
    * rules/order-in-models: Include correct position in warning
    
    * rules/order-in-routes: Include correct position in warning

--- tests/lib/rules/order-in-controllers.js
+++ tests/lib/rules/order-in-controllers.js
@@ -15,63 +15,114 @@ var eslintTester = new RuleTester();
 eslintTester.run('order-in-controllers', rule, {
   valid: [
     {
-      code: 'export default Controller.extend();',
+      code: `export default Controller.extend();`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
     },
     {
-      code: 'export default Controller.extend({currentUser: service(), queryParams: [], customProp: "test", actions: {}, _customAction() {}, _customAction2: function() {}, tSomeTask: task(function* () {}) });',
+      code: `export default Controller.extend({
+        currentUser: service(),
+        queryParams: [],
+        customProp: "test",
+        actions: {},
+        _customAction() {},
+        _customAction2: function() {},
+        tSomeTask: task(function* () {})
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
     },
     {
-      code: 'export default Controller.extend({queryParams: [], customProp: "test", comp: computed("test", function() {}), obs: observer("asd", function() {}), actions: {} });',
+      code: `export default Controller.extend({
+        queryParams: [],
+        customProp: "test",
+        comp: computed("test", function() {}),
+        obs: observer("asd", function() {}),
+        actions: {}
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
     },
     {
-      code: 'export default Controller.extend({customProp: "test", comp: computed("test", function() {}), comp: computed("test", function() {\n}), actions: {}, _customAction() {} });',
+      code: `export default Controller.extend({
+        customProp: "test",
+        comp: computed("test", function() {}),
+        comp2: computed("test", function() {
+        }),
+        actions: {},
+        _customAction() {}
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
     },
   ],
   invalid: [
     {
-      code: 'export default Controller.extend({queryParams: [], currentUser: service() });',
+      code: `export default Controller.extend({
+        queryParams: [],
+        currentUser: service()
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
       errors: [{
-        message: 'Check order of properties',
+        message: 'The service injection should be above the default property on line 2',
+        line: 3
       }],
     },
     {
-      code: 'export default Controller.extend({currentUser: service(), customProp: "test", queryParams: [] });',
+      code: `export default Controller.extend({
+        currentUser: service(),
+        customProp: "test",
+        queryParams: []
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
       errors: [{
-        message: 'Check order of properties',
+        message: 'The default property should be above the property on line 3',
+        line: 4
       }],
     },
     {
-      code: 'export default Controller.extend({queryParams: [], actions: {}, customProp: "test" });',
+      code: `export default Controller.extend({
+        queryParams: [],
+        actions: {},
+        customProp: "test"
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
       errors: [{
-        message: 'Check order of properties',
+        message: 'The property should be above the actions hash on line 3',
+        line: 4
       }],
     },
     {
-      code: 'export default Controller.extend({queryParams: [], _customAction() {}, actions: {} });',
+      code: `export default Controller.extend({
+        queryParams: [],
+        _customAction() {},
+        actions: {}
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
       errors: [{
-        message: 'Check order of properties',
+        message: 'The actions hash should be above the custom method on line 3',
+        line: 4
       }],
     },
     {
-      code: 'export default Controller.extend({test: "asd", queryParams: [], actions: {} });',
+      code: `export default Controller.extend({
+        test: "asd",
+        queryParams: [],
+        actions: {}
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
       errors: [{
-        message: 'Check order of properties',
+        message: 'The default property should be above the property on line 2',
+        line: 3
       }],
     },
     {
-      code: 'export default Controller.extend({test: "asd", obs: observer("asd", function() {}), comp: computed("asd", function() {}), actions: {} });',
+      code: `export default Controller.extend({
+        test: "asd",
+        obs: observer("asd", function() {}),
+        comp: computed("asd", function() {}),
+        actions: {}
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
       errors: [{
-        message: 'Check order of properties',
+        message: 'The single-line function should be above the observer on line 3',
+        line: 4
       }],
     },
   ]
--- tests/lib/rules/order-in-routes.js
+++ tests/lib/rules/order-in-routes.js
@@ -15,78 +15,156 @@ var eslintTester = new RuleTester();
 eslintTester.run('order-in-routes', rule, {
   valid: [
     {
-      code: 'export default Route.extend();',
+      code: `export default Route.extend();`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
     },
     {
-      code: 'export default Route.extend({currentUser: service(), queryParams: {}, customProp: "test", model() {}, beforeModel() {}, actions: {}, _customAction() {}, _customAction2: function() {}, tSomeTask: task(function* () {}) });',
+      code: `export default Route.extend({
+        currentUser: service(),
+        queryParams: {},
+        customProp: "test",
+        model() {},
+        beforeModel() {},
+        actions: {},
+        _customAction() {},
+        _customAction2: function() {},
+        tSomeTask: task(function* () {})
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
     },
     {
-      code: 'export default Route.extend({model() {}, actions: { test() { return this._customAction() } }, _customAction() {} });',
+      code: `export default Route.extend({
+        model() {},
+        actions: { 
+          test() { return this._customAction() }
+        },
+        _customAction() {}
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
     },
     {
-      code: 'export default Route.extend({model() {}, render() {}, init() {} });',
+      code: `export default Route.extend({
+        model() {},
+        render() {},
+        init() {}
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
     },
     {
-      code: 'export default Route.extend({mergedProperties: {}, model() {}, actions: {} });',
+      code: `export default Route.extend({
+        mergedProperties: {},
+        model() {},
+        actions: {}
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
     },
     {
-      code: 'export default Route.extend({mergedProperties: {}, test: "asd", model() {} });',
+      code: `export default Route.extend({
+        mergedProperties: {},
+        test: "asd",
+        model() {}
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
     },
   ],
   invalid: [
     {
-      code: 'export default Route.extend({queryParams: {}, currentUser: service(), customProp: "test", model() {}, beforeModel() {}, actions: {}, _customAction() {} });',
+      code: `export default Route.extend({
+        queryParams: {},
+        currentUser: service(),
+        customProp: "test",
+        model() {},
+        beforeModel() {},
+        actions: {},
+        _customAction() {}
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
       errors: [{
-        message: 'Check order of properties',
+        message: 'The service injection should be above the default property on line 2',
+        line: 3,
       }],
     },
     {
-      code: 'export default Route.extend({customProp: "test", queryParams: {}, model() {}, beforeModel() {}, actions: {}, _customAction() {} });',
+      code: `export default Route.extend({
+        customProp: "test",
+        queryParams: {},
+        model() {},
+        beforeModel() {},
+        actions: {},
+        _customAction() {}
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
       errors: [{
-        message: 'Check order of properties',
+        message: 'The default property should be above the property on line 2',
+        line: 3,
       }],
     },
     {
-      code: 'export default Route.extend({customProp: "test", queryParams: {}, beforeModel() {}, model() {}, actions: {}, _customAction() {} });',
+      code: `export default Route.extend({
+        customProp: "test",
+        queryParams: {},
+        beforeModel() {},
+        model() {},
+        actions: {},
+        _customAction() {}
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
       errors: [{
-        message: 'Check order of properties',
+        message: 'The default property should be above the property on line 2',
+        line: 3,
+      }, {
+        message: 'The model hook should be above the lifecycle hook on line 4',
+        line: 5,
       }],
     },
     {
-      code: 'export default Route.extend({queryParams: {}, customProp: "test", model() {}, _customAction() {}, actions: {} });',
+      code: `export default Route.extend({
+        queryParams: {},
+        customProp: "test",
+        model() {},
+        _customAction() {},
+        actions: {}
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
       errors: [{
-        message: 'Check order of properties',
+        message: 'The actions hash should be above the custom method on line 5',
+        line: 6,
       }],
     },
     {
-      code: 'export default Route.extend({model() {}, customProp: "test", actions: {} });',
+      code: `export default Route.extend({
+        model() {},
+        customProp: "test",
+        actions: {}
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
       errors: [{
-        message: 'Check order of properties',
+        message: 'The property should be above the model hook on line 2',
+        line: 3,
       }],
     },
     {
-      code: 'export default Route.extend({test: "asd", mergedProperties: {}, model() {} });',
+      code: `export default Route.extend({
+        test: "asd",
+        mergedProperties: {},
+        model() {}
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
       errors: [{
-        message: 'Check order of properties',
+        message: 'The default property should be above the property on line 2',
+        line: 3,
       }],
     },
     {
-      code: 'export default Route.extend({test: "asd", _test2() {}, model() {} });',
+      code: `export default Route.extend({
+        test: "asd",
+        _test2() {},
+        model() {}
+      });`,
       parserOptions: {ecmaVersion: 6, sourceType: "module"},
       errors: [{
-        message: 'Check order of properties',
+        message: 'The model hook should be above the custom method on line 3',
+        line: 4,
       }],
     },
   ]
