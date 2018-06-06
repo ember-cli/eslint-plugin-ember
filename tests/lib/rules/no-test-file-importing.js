b4f447a347d6ea1f9aa2ba5cf1dd123d658ff69d
Stephen Yeung <syeung@linkedin.com>
2018-06-06 11:56:59 -0700

    Adding no-test-file-importing rule

---
+++ tests/lib/rules/no-test-file-importing.js
@@ -0,0 +1,66 @@
+const rule = require('../../../lib/rules/no-test-file-importing');
+const MESSAGE = rule.meta.message;
+const RuleTester = require('eslint').RuleTester;
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module'
+  }
+});
+
+ruleTester.run('no-test-file-importing', rule, {
+  valid: [
+    {
+      code: `
+        import setupModule from './some-test-helper';
+        import { module, test } from 'qunit';
+
+        module('Acceptance | module', setupModule());
+      `
+    }
+  ],
+  invalid: [
+    {
+      code: `
+        import setupModule from './some-other-test';
+        import { module, test } from 'qunit';
+
+        module('Acceptance | module', setupModule());
+      `,
+      errors: [
+        {
+          message: MESSAGE
+        }
+      ]
+    },
+    {
+      code: `
+        import {
+          beforeEachSetup,
+          testName,
+        } from './some-other-test';
+        import { module, test } from 'qunit';
+
+        module('Acceptance | module', beforeEachSetup());
+      `,
+      errors: [
+        {
+          message: MESSAGE
+        }
+      ]
+    },
+    {
+      code: `
+        import testModule from '../../test-dir/another-test';
+        import { module, test } from 'qunit';
+
+        module('Acceptance | module', testModule());
+      `,
+      errors: [
+        {
+          message: MESSAGE
+        }
+      ]
+    }
+  ]
+});
