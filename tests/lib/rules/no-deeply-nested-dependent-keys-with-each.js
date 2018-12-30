35492bd8f71e7f9f2acbc6c5cc2b5080762f2c91
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2018-12-30 10:47:24 -0500

    fix: false positive for 'no-deeply-nested-dependent-keys-with-each' rule.

--- tests/lib/rules/no-deeply-nested-dependent-keys-with-each.js
+++ tests/lib/rules/no-deeply-nested-dependent-keys-with-each.js
@@ -16,27 +16,35 @@ ruleTester.run('no-deeply-nested-dependent-keys-with-each', rule, {
   valid: [
     'Ember.computed(function() {})',
     "Ember.computed('foo', function() {})",
+    "Ember.computed('foo', function() {}).readOnly()",
     "Ember.computed('foo.bar', function() {})",
     "Ember.computed('foo.bar.@each.baz', function() {})",
     "Ember.computed('foo.@each.bar', function() {})",
     "Ember.computed('foo.@each.{bar,baz}', function() {})",
     'computed(function() {})',
     "computed('foo', function() {})",
+    "computed('foo', function() {}).readOnly()",
     "computed('foo.bar', function() {})",
     "computed('foo.bar.@each.baz', function() {})",
     "computed('foo.@each.bar', function() {})",
     "computed('foo.@each.{bar,baz}', function() {})",
     "computed('foo.@each.{bar,baz}', function() {})",
 
-    // Not Ember's `computed`:
+    // Not Ember's `computed` function:
     "otherClass.computed('foo.@each.bar.baz', function() {})",
     "otherClass.myFunction('foo.@each.bar.baz', function() {})",
     "myFunction('foo.@each.bar.baz', function() {})",
-    "Ember.myFunction('foo.@each.bar.baz', function() {})"
+    "Ember.myFunction('foo.@each.bar.baz', function() {})",
+    "computed.unrelatedFunction('foo.@each.bar.baz', function() {})",
+    "Ember.computed.unrelatedFunction('foo.@each.bar.baz', function() {})"
   ],
   invalid: [
     {
       code: "Ember.computed('foo.@each.bar.baz', function() {})",
+      errors: [{ message: ERROR_MESSAGE, type: 'CallExpression' }]
+    },
+    {
+      code: "Ember.computed('foo.@each.bar.baz', function() {}).readOnly()",
       errors: [{ message: ERROR_MESSAGE, type: 'CallExpression' }]
     },
     {
@@ -49,6 +57,10 @@ ruleTester.run('no-deeply-nested-dependent-keys-with-each', rule, {
     },
     {
       code: "computed('foo.@each.bar.baz', function() {})",
+      errors: [{ message: ERROR_MESSAGE, type: 'CallExpression' }]
+    },
+    {
+      code: "computed('foo.@each.bar.baz', function() {}).readOnly()",
       errors: [{ message: ERROR_MESSAGE, type: 'CallExpression' }]
     },
     {
