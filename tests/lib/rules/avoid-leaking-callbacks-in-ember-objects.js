08625d0ca9c32dc3fa8f5123e3155e52b93748f6
Rajasegar Chandran <rajasegar.c@gmail.com>
2019-06-04 12:55:15 +0530

    [CHORE] Addressing review comments - phase 2
    1. Restoing Ember Object section in Readme
    2. Rearranging the new rule in lib/index based on alphabetical order
    3. Removing commented code in the rule
    4. Removing the recommended flag from the rule
    5. Removing the rule from snapshot recommended
    6. Removing the duplicate valid test case with Ember.Component.extend

--- tests/lib/rules/avoid-leaking-callbacks-in-ember-objects.js
+++ tests/lib/rules/avoid-leaking-callbacks-in-ember-objects.js
@@ -21,22 +21,6 @@ eslintTester.run('avoid-leaking-callbacks-in-ember-objects', rule, {
   valid: [
     {
       code: `
-export default Ember.Component.extend({
-  didInsertElement() {
-    if (this.get('onScroll')) {
-      this._onScrollHandler = (...args) => this.get('onScroll')(...args);
-      window.addEventListener('scroll', this._onScrollHandler);
-    }
-  },
-
-  willDestroyElement() {
-    window.removeEventListener("scroll", this._onScrollHandler);
-    this._super(...arguments);
-  }
-});`,
-    },
-    {
-      code: `
 import Component from '@ember/component';
 
 export default Component.extend({
