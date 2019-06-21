4b493afccf50a477b4dd35d402a7945321c4ec08
Chris Garrett <cgarrett@linkedin.com>
2019-06-20 17:06:30 -0700

    [FEATURE] Adds classic decorator rules
    Adds two rules for users who are using the `@classic` decorator to help
    with upgrading to native classes and Ember Octane.

--- README.md
+++ README.md
@@ -130,6 +130,8 @@ The `--fix` option on the command line automatically fixes problems reported by 
 |    | Rule ID | Description |
 |:---|:--------|:------------|
 | :white_check_mark: | [avoid-leaking-state-in-ember-objects](./docs/rules/avoid-leaking-state-in-ember-objects.md) | Avoids state leakage |
+|  | [classic-decorator-hooks](./docs/rules/classic-decorator-hooks.md) | Ensure correct hooks are used for both classic and non-classic classes |
+|  | [classic-decorator-no-classic-methods](./docs/rules/classic-decorator-no-classic-methods.md) | Prevent usage of classic APIs such as get/set in classes that aren't explicitly decorated with @classic |
 |  | [computed-property-getters](./docs/rules/computed-property-getters.md) | Enforce the consistent use of getters in computed properties |
 
 
---
+++ tests/lib/rules/classic-decorator-hooks.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const rule = require('../../../lib/rules/classic-decorator-hooks');
+const RuleTester = require('eslint').RuleTester;
+
+const {
+  ERROR_MESSAGE_INIT_IN_NON_CLASSIC,
+  ERROR_MESSAGE_DESTROY_IN_NON_CLASSIC,
+  ERROR_MESSAGE_CONSTRUCTOR_IN_CLASSIC,
+} = rule;
+
+const ruleTester = new RuleTester({
+  parser: 'babel-eslint',
+  parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+});
+
+ruleTester.run('classic-decorator-hooks', rule, {
+  valid: [
+    `
+      const Foo = EmberObject.extend({
+        init() {},
+        destroy() {}
+      })
+    `,
+    `
+      class Foo extends Bar {
+        constructor() {}
+        willDestroy() {}
+      }
+    `,
+    `
+      @classic
+      class Foo extends Bar {
+        init() {}
+        destroy() {}
+      }
+    `,
+    `
+      class Foo {
+        init() {}
+        destroy() {}
+      }
+    `,
+  ],
+
+  invalid: [
+    {
+      code: `
+        class Foo extends Bar {
+          init() {}
+        }
+      `,
+      errors: [
+        {
+          message: ERROR_MESSAGE_INIT_IN_NON_CLASSIC,
+        },
+      ],
+    },
+    {
+      code: `
+        class Foo extends Bar {
+          destroy() {}
+        }
+      `,
+      errors: [
+        {
+          message: ERROR_MESSAGE_DESTROY_IN_NON_CLASSIC,
+        },
+      ],
+    },
+    {
+      code: `
+        @classic
+        class Foo extends Bar {
+          constructor() {}
+        }
+      `,
+      errors: [
+        {
+          message: ERROR_MESSAGE_CONSTRUCTOR_IN_CLASSIC,
+        },
+      ],
+    },
+  ],
+});
