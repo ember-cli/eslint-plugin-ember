258a746c9580c7ffe2e0952b37400c941fec45fd
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2019-09-21 12:32:22 -0700

    internal: cleanup/alphabetize lint configuration
    No functional change. Just reorganizing it.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,22 +5,29 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2017,
   },
-  plugins: ['eslint-plugin', 'import', 'prettier'],
-  extends: ['eslint:recommended', 'plugin:eslint-plugin/all', 'plugin:import/errors', 'plugin:import/warnings', 'prettier'],
+  plugins: [
+    'eslint-plugin',
+    'import',
+    'prettier'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:eslint-plugin/all',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'prettier'
+  ],
   env: {
     es6: true,
     node: true,
     jest: true,
   },
   rules: {
-    // Possible Errors
-    'no-async-promise-executor': 'error',
-    'no-template-curly-in-string': 'error',
-    'require-atomic-updates': 'error',
-
-    // Best Practices
+    // Optional eslint rules:
     'array-callback-return': 'error',
     eqeqeq: 'error',
+    'no-async-promise-executor': 'error',
+    'no-duplicate-imports': 'error',
     'no-eval': 'error',
     'no-extend-native': 'error',
     'no-extra-bind': 'error',
@@ -33,31 +40,32 @@ module.exports = {
     'no-return-assign': 'error',
     'no-self-compare': 'error',
     'no-sequences': 'error',
+    'no-template-curly-in-string': 'error',
     'no-throw-literal': 'error',
     'no-unused-expressions': 'error',
     'no-useless-call': 'error',
     'no-useless-catch': 'error',
+    'no-useless-computed-key': 'error',
     'no-useless-concat': 'error',
+    'no-useless-constructor': 'error',
     'no-useless-escape': 'error',
+    'no-useless-rename': 'error',
     'no-useless-return': 'error',
+    'no-var': 'error',
     'no-void': 'error',
     'no-with': 'error',
+    'object-shorthand': 'error',
+    'prefer-numeric-literals': 'error',
     'prefer-promise-reject-errors': 'error',
     radix: 'error',
+    'require-atomic-updates': 'error',
     'wrap-iife': 'error',
     yoda: 'error',
 
-    // ECMAScript 6
-    'no-duplicate-imports': 'error',
-    'no-useless-computed-key': 'error',
-    'no-useless-constructor': 'error',
-    'no-useless-rename': 'error',
-    'no-var': 'error',
-    'object-shorthand': 'error',
-    'prefer-numeric-literals': 'error',
-
+    // Prettier:
     'prettier/prettier': 'error',
 
+    // eslint-plugin rules:
     'eslint-plugin/no-deprecated-report-api': 'off',
     'eslint-plugin/require-meta-type': 'off',
     'eslint-plugin/require-meta-docs-url': 'off',
