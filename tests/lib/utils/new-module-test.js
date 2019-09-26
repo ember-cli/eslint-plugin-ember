665d1f0e39c4d0efc69db9590ed50b73e206f8eb
Cyrille David <cyrille@qonto.eu>
2019-09-26 13:07:32 +0200

    Add a test for buildFix

--- tests/lib/utils/new-module-test.js
+++ tests/lib/utils/new-module-test.js
@@ -1,7 +1,13 @@
 'use strict';
 
 const babelEslint = require('babel-eslint');
-const { isDestructuring } = require('../../../lib/utils/new-module');
+const { buildFix, isDestructuring } = require('../../../lib/utils/new-module');
+
+const modulesData = {
+  'ember-data/model': {
+    default: ['@ember-data/model'],
+  },
+};
 
 describe('isDestructuring', () => {
   it('should check a destructuring import', () => {
@@ -17,5 +23,13 @@ describe('isDestructuring', () => {
   it('should check a ForInStatement', () => {
     const node = babelEslint.parse(`for (const item in list) {};`).body[0].left.declarations[0];
     expect(isDestructuring(node)).toBeFalsy();
+  });
+});
+
+describe('buildFix', () => {
+  it('returns a function', () => {
+    const node = babelEslint.parse(`import Model from "ember-data/model"`).body[0];
+    const fix = buildFix(node, modulesData);
+    expect(typeof fix === 'function').toBeTruthy();
   });
 });
