cdff5032e11e19c700c26505cc6d50aa797fa259
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-10-10 05:28:07 -0700

    split out tests, add configuration documentation and add invalid scenarios

--- tests/lib/rules/no-arrow-function-computed-properties.js
+++ tests/lib/rules/no-arrow-function-computed-properties.js
@@ -19,26 +19,37 @@ const ruleTester = new RuleTester({
 });
 ruleTester.run('no-arrow-function-computed-properties', rule, {
   valid: [
+    'computed()',
+    'computed(function() { return 123; })',
+    'computed("prop", function() { return this.prop; })',
+    'computed("prop", function() { return this.prop; }).volatile()',
+    'computed.map("products", function(product) { return someFunction(product); })',
+    'other(() => {})',
+    'other.computed(() => {})',
     {
-      code: `
-        computed('prop', function() { return this.prop; });
-        computed('prop', function() { return this.prop; }).volatile();
-        computed(() => { return 123; });
-        computed(() => { return "string stuff"; });
-        computed(() => []);
-        computed.map('products', product => { return someFunction(product); });
-      `,
+      code: `computed('prop', function() { return this.prop; });`,
       options: [{ onlyThisContexts: true }],
     },
-    `
-    computed();
-    computed(function() { return 123; });
-    computed('prop', function() { return this.prop; });
-    computed('prop', function() { return this.prop; }).volatile();
-    computed.map('products', function(product) { return someFunction(product); });
-    other(() => {});
-    other.computed(() => {});
-    `,
+    {
+      code: `computed('prop', function() { return this.prop; }).volatile();`,
+      options: [{ onlyThisContexts: true }],
+    },
+    {
+      code: `computed(() => { return 123; });`,
+      options: [{ onlyThisContexts: true }],
+    },
+    {
+      code: `computed(() => { return "string stuff"; });`,
+      options: [{ onlyThisContexts: true }],
+    },
+    {
+      code: `computed(() => []);`,
+      options: [{ onlyThisContexts: true }],
+    },
+    {
+      code: `computed.map('products', product => { return someFunction(product); });`,
+      options: [{ onlyThisContexts: true }],
+    },
   ],
   invalid: [
     {
@@ -57,6 +68,21 @@ ruleTester.run('no-arrow-function-computed-properties', rule, {
     {
       code: "computed.map('products', product => { return someFunction(product); })",
       errors: [{ message: ERROR_MESSAGE, type: 'ArrowFunctionExpression' }],
+    },
+    {
+      code: 'computed(() => { return 123; })',
+      errors: [],
+      options: [{ onlyThisContexts: true }],
+    },
+    {
+      code: "computed('prop', () => { return this.prop; })",
+      errors: [{ message: ERROR_MESSAGE, type: 'ArrowFunctionExpression' }],
+      options: [{ onlyThisContexts: true }],
+    },
+    {
+      code: "computed('prop', () => { return this.prop; }).volatile()",
+      errors: [{ message: ERROR_MESSAGE, type: 'ArrowFunctionExpression' }],
+      options: [{ onlyThisContexts: true }],
     },
   ],
 });
