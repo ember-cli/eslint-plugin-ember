f9b24703d25b09af2e3a89bf8d3bd5bd44083916
em <ernesto.mudu@gmail.com>
2020-04-14 20:44:30 -0700

    Update rule name and add check for clearRender.

--- tests/lib/rules/no-test-this-render.js
+++ tests/lib/rules/no-test-this-render.js
@@ -4,7 +4,7 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-const rule = require('../../../lib/rules/no-this-render');
+const rule = require('../../../lib/rules/no-test-this-render');
 const RuleTester = require('eslint').RuleTester;
 
 const { ERROR_MESSAGE } = rule;
@@ -34,11 +34,23 @@ ruleTester.run('no-this-render', rule, {
     },
     {
       filename: TEST_FILE_NAME,
+      code: 'myCustomClass.clearRender(myFunction);',
+    },
+    {
+      filename: TEST_FILE_NAME,
       code: 'render.otherFunction(myFunction);',
+    },
+    {
+      filename: TEST_FILE_NAME,
+      code: 'clearRender.otherFunction(myFunction);',
     },
     {
       filename: 'not-a-test-file.js',
       code: 'async () => { await this.render(); }',
+    },
+    {
+      filename: 'not-a-test-file.js',
+      code: 'async () => { await this.clearRender(); }',
     },
   ],
   invalid: [
@@ -53,6 +65,22 @@ ruleTester.run('no-this-render', rule, {
       code: `
         test('foo', async function(assert) {
           await this.render();
+        });
+      `,
+      output: null,
+      errors: [{ message: ERROR_MESSAGE, type: 'CallExpression' }],
+    },
+    {
+      filename: TEST_FILE_NAME,
+      code: 'async () => { await this.clearRender(); }',
+      output: null,
+      errors: [{ message: ERROR_MESSAGE, type: 'CallExpression' }],
+    },
+    {
+      filename: TEST_FILE_NAME,
+      code: `
+        test('foo', async function(assert) {
+          await this.clearRender();
         });
       `,
       output: null,
