e3d1b3f41e296e623188c01887427704bdb67398
Rustam Goygov <GoygovRustam@gmail.com>
2020-05-20 12:00:24 -0700

    no-ember-super-in-es-classes lint rule's message should include the correct syntax

--- tests/lib/rules/no-ember-super-in-es-classes.js
+++ tests/lib/rules/no-ember-super-in-es-classes.js
@@ -24,50 +24,50 @@ eslintTester.run('no-ember-super-in-es-classes', rule, {
       code: 'class Foo { init() { this._super(); } }',
       output: 'class Foo { init() { super.init(); } }',
       errors: [
-        { message: "Don't use `this._super` in ES classes; instead, you should use super()" },
+        { message: "Don't use `this._super` in ES classes; instead, you should use `super`" },
       ],
     },
     {
       code: 'class Foo { init(a, b) { this._super(a); } }',
       output: 'class Foo { init(a, b) { super.init(a); } }',
       errors: [
-        { message: "Don't use `this._super` in ES classes; instead, you should use super()" },
+        { message: "Don't use `this._super` in ES classes; instead, you should use `super`" },
       ],
     },
     {
       code: 'class Foo { init() { this._super(...arguments); } }',
       output: 'class Foo { init() { super.init(...arguments); } }',
       errors: [
-        { message: "Don't use `this._super` in ES classes; instead, you should use super()" },
+        { message: "Don't use `this._super` in ES classes; instead, you should use `super`" },
       ],
     },
     {
       code: 'class Foo { init() { this._super.apply(this, arguments); } }',
       output: 'class Foo { init() { super.init.apply(this, arguments); } }',
       errors: [
-        { message: "Don't use `this._super` in ES classes; instead, you should use super()" },
+        { message: "Don't use `this._super` in ES classes; instead, you should use `super`" },
       ],
     },
     {
       code: 'class Foo { init() { if (x) { this._super(1); } else { this._super(2); } } }',
       output: 'class Foo { init() { if (x) { super.init(1); } else { super.init(2); } } }',
       errors: [
-        { message: "Don't use `this._super` in ES classes; instead, you should use super()" },
-        { message: "Don't use `this._super` in ES classes; instead, you should use super()" },
+        { message: "Don't use `this._super` in ES classes; instead, you should use `super`" },
+        { message: "Don't use `this._super` in ES classes; instead, you should use `super`" },
       ],
     },
     {
       code: 'class Foo { "a b"() { this._super(); } }',
       output: 'class Foo { "a b"() { super["a b"](); } }',
       errors: [
-        { message: "Don't use `this._super` in ES classes; instead, you should use super()" },
+        { message: "Don't use `this._super` in ES classes; instead, you should use `super`" },
       ],
     },
     {
       code: 'class Foo { [Symbol.iterator]() { this._super(); } }',
       output: 'class Foo { [Symbol.iterator]() { super[Symbol.iterator](); } }',
       errors: [
-        { message: "Don't use `this._super` in ES classes; instead, you should use super()" },
+        { message: "Don't use `this._super` in ES classes; instead, you should use `super`" },
       ],
     },
   ],
