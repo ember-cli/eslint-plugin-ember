848c11d920c17878843fb1bc762240851f936ae5
Tobias Bieniek <tobias@bieniek.cloud>
2020-10-25 18:01:17 +0100

    no-settled-after-test-helper: Support import renaming

--- tests/lib/rules/no-settled-after-test-helper.js
+++ tests/lib/rules/no-settled-after-test-helper.js
@@ -14,32 +14,52 @@ const ruleTester = new RuleTester({
 ruleTester.run('no-settled-after-test-helper', rule, {
   valid: [
     `
+      import { waitFor, settled } from '@ember/test-helpers';
+
       async () => {
         await waitFor('.foo');
         await settled();
       }
     `,
     `
+      import { waitFor, settled } from '@ember/test-helpers';
+
       async () => {
         await waitFor('.foo');
         await settled;
       }
     `,
     `
+      import { settled } from '@ember/test-helpers';
+
       async () => {
         await settled();
       }
     `,
     `
+      import { settled } from '@ember/test-helpers';
+
       async () => {
         this.set('foo', 42);
         await settled();
       }
     `,
     `
+      import { settled } from '@ember/test-helpers';
+
       async () => {
         let promise = waitFor('.foo');
         await promise;
+        await settled();
+      }
+    `,
+    `
+      import { settled } from '@ember/test-helpers';
+
+      function click() {}
+
+      async () => {
+        await click('.foo');
         await settled();
       }
     `,
@@ -47,48 +67,79 @@ ruleTester.run('no-settled-after-test-helper', rule, {
   invalid: [
     {
       code: `
+        import { click, settled } from '@ember/test-helpers';
+
         async () => {
           await click('.foo');
           await settled();
         }
       `,
       output: `
+        import { click, settled } from '@ember/test-helpers';
+
         async () => {
           await click('.foo');
 \u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020
         }
       `,
-      errors: [{ message: ERROR_MESSAGE, line: 4, column: 11, endLine: 4, endColumn: 27 }],
+      errors: [{ message: ERROR_MESSAGE, line: 6, column: 11, endLine: 6, endColumn: 27 }],
     },
     {
       code: `
+        import { fillIn, settled } from '@ember/test-helpers';
+
         async () => {
           await fillIn('.foo');
           await settled();
         }
       `,
       output: `
+        import { fillIn, settled } from '@ember/test-helpers';
+
         async () => {
           await fillIn('.foo');
 \u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020
         }
       `,
-      errors: [{ message: ERROR_MESSAGE, line: 4, column: 11, endLine: 4, endColumn: 27 }],
+      errors: [{ message: ERROR_MESSAGE, line: 6, column: 11, endLine: 6, endColumn: 27 }],
     },
     {
       code: `
+        import { settled } from '@ember/test-helpers';
+
         async () => {
           await settled();
           await settled();
         }
       `,
       output: `
+        import { settled } from '@ember/test-helpers';
+
         async () => {
           await settled();
 \u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020
         }
       `,
-      errors: [{ message: ERROR_MESSAGE, line: 4, column: 11, endLine: 4, endColumn: 27 }],
+      errors: [{ message: ERROR_MESSAGE, line: 6, column: 11, endLine: 6, endColumn: 27 }],
+    },
+    {
+      code: `
+        import { click as cliiiick, settled } from '@ember/test-helpers';
+
+        async () => {
+          await cliiiick('.foo');
+          await settled();
+        }
+      `,
+      output: `
+        import { click as cliiiick, settled } from '@ember/test-helpers';
+
+        async () => {
+          await cliiiick('.foo');
+\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020\u0020
+        }
+      `,
+      errors: [{ message: ERROR_MESSAGE, line: 6, column: 11, endLine: 6, endColumn: 27 }],
     },
   ],
 });
