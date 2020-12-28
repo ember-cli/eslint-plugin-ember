4cd66b219bb0423e4bd999a04372d789faea1936
Tobias Bieniek <tobias@bieniek.cloud>
2020-12-28 14:09:59 +0100

    Implement `require-fetch-import` rule

---
+++ tests/lib/rules/require-fetch-import.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const rule = require('../../../lib/rules/require-fetch-import');
+const RuleTester = require('eslint').RuleTester;
+
+const { ERROR_MESSAGE } = rule;
+
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+});
+
+ruleTester.run('require-fetch-import', rule, {
+  valid: [
+    `
+      import fetch from 'fetch';
+
+      fetch('/something');
+    `,
+    `
+      fetch('/something');
+
+      import fetch from 'fetch';
+    `,
+    `
+      import { default as fetch } from 'fetch';
+
+      fetch('/something');
+    `,
+    "foo('/something');",
+  ],
+
+  invalid: [
+    {
+      code: "fetch('/something');",
+      output: null,
+      errors: [
+        {
+          message: ERROR_MESSAGE,
+        },
+      ],
+    },
+  ],
+});
