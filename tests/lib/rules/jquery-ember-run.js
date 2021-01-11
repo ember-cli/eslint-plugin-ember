44363abda7ec5f011b477b5fd9a3262082ce53ad
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2021-01-11 12:18:58 -0500

    test: improve tests for jquery-ember-run rule

--- tests/lib/rules/jquery-ember-run.js
+++ tests/lib/rules/jquery-ember-run.js
@@ -9,24 +9,63 @@ const RuleTester = require('eslint').RuleTester;
 // Tests
 // ------------------------------------------------------------------------------
 
+const { ERROR_MESSAGE } = rule;
 const eslintTester = new RuleTester({
   parserOptions: { ecmaVersion: 6, sourceType: 'module' },
 });
 
 eslintTester.run('jquery-ember-run', rule, {
   valid: [
-    'Ember.$("#something-rendered-by-jquery-plugin").on("click", () => {Ember.run.bind(this, this._handlerActionFromController);});',
+    // jQuery/bind from Ember
+    `import Ember from "ember";
+     Ember.$("#item").on("click", () => { Ember.run.bind(this, this.handle); });`,
+
+    // jQuery from Ember with destructuring
+    `import Ember from "ember";
+     const { $ } = Ember;
+     $("#item").on("click", () => { Ember.run.bind(this, this.handle); });`,
+
+    // Global jQuery
+    `import { bind } from "@ember/runloop";
+     $("#item").on("click", () => { bind(this, this.handle); });`,
+
+    // Imported jQuery
+    `import { bind } from "@ember/runloop";
+     import $ from "jquery";
+     $("#item").on("click", () => { bind(this, this.handle); });`,
+
+    // No callback
+    '$("#item");',
+    '$("#item").on("click");',
+
+    // Callback but not jQuery
+    'notJquery("#item").on("click", () => {this.handle();});',
   ],
   invalid: [
     {
-      code:
-        'Ember.$("#something-rendered-by-jquery-plugin").on("click", () => {this._handlerActionFromController();});',
+      // jQuery from Ember
+      code: 'import Ember from "ember"; Ember.$("#item").on("click", () => { this.handle(); });',
       output: null,
-      errors: [
-        {
-          message: "Don't use jQuery without Ember Run Loop",
-        },
-      ],
+      errors: [{ message: ERROR_MESSAGE, type: 'MemberExpression' }],
+    },
+    {
+      // jQuery from Ember with destructuring
+      code:
+        'import Ember from "ember"; const { $ } = Ember; $("#item").on("click", () => { this.handle(); });',
+      output: null,
+      errors: [{ message: ERROR_MESSAGE, type: 'MemberExpression' }],
+    },
+    {
+      // Global jQuery
+      code: '$("#item").on("click", () => {this.handle();});',
+      output: null,
+      errors: [{ message: ERROR_MESSAGE, type: 'MemberExpression' }],
+    },
+    {
+      // Imported jQuery
+      code: 'import $ from "jquery"; $("#item").on("click", () => { this.handle(); });',
+      output: null,
+      errors: [{ message: ERROR_MESSAGE, type: 'MemberExpression' }],
     },
   ],
 });
