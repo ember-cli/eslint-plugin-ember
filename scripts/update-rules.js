6554d3812a9f359f0fd2a7af59f1bd731eb090b0
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2022-07-20 15:08:04 -0700

    fix lint

--- scripts/update-rules.js
+++ scripts/update-rules.js
@@ -91,7 +91,6 @@ ${deprecatedRules
 `;
 }
 
-// eslint-disable-next-line unicorn/prefer-object-from-entries
 const recommendedRules = rules.reduce((obj, entry) => {
   const name = `ember/${entry[0]}`;
   const recommended = entry[1].meta.docs.recommended;
