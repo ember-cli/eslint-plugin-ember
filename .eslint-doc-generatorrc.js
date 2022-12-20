fa5510feacad8e27cc12f0f9279478f1083b82f8
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2022-12-20 12:09:49 -0500

    chore: switch to config file for eslint-doc-generator

---
+++ .eslint-doc-generatorrc.js
@@ -0,0 +1,7 @@
+/** @type {import('eslint-doc-generator').GenerateOptions} */
+module.exports = {
+  ruleDocSectionInclude: ['Examples'],
+  ruleDocTitleFormat: 'prefix-name',
+  ruleListSplit: 'meta.docs.category',
+  urlConfigs: 'https://github.com/ember-cli/eslint-plugin-ember#-configurations',
+};
