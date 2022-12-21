6a5007883dc8e02259fa89c700a3cd04c4ef67dc
Ryan Tablada <ryan.tablada@gmail.com>
2022-12-21 12:06:54 -0600

    Implement no-implicit-injections lint rule
    chore: generate docs
    
    wip: setup for inital create
    
    Implement basic identification for implicit injections
    
    fix: failure to recognize module type or all modules
    
    feat: add fixing for no-implicit-injections
    
    update readme for no-implicit-injections

---
+++ docs/rules/no-implicit-injections.md
@@ -0,0 +1,121 @@
+# ember/no-implicit-injections
+
+💼 This rule is enabled in the ✅ `recommended` [config](https://github.com/ember-cli/eslint-plugin-ember#-configurations).
+
+🔧 This rule is automatically fixable by the [`--fix` CLI option](https://eslint.org/docs/latest/user-guide/command-line-interface#--fix).
+
+<!-- end auto-generated rule header -->
+
+✅ The `"extends": "plugin:ember/recommended"` property in a configuration file enables this rule.
+
+🔧 The `--fix` option on the [command line](https://eslint.org/docs/user-guide/command-line-interface#fixing-problems) can automatically fix some of the problems reported by this rule.
+
+Ember 3.26 introduced a deprecation for relying on implicit service injections or allowing addons to implicitly inject services into all classes of certain types. Support for this is dropped in Ember 4.0.
+
+In many applications, `this.store` from Ember Data is often used without injecting the `store` service in Controllers or Routes. Other addons may also have included implicit service injections via initializers and the `application.inject` API.
+
+To resolve this deprecation, a service should be explicitly declared and injected using the [service injection decorator](https://api.emberjs.com/ember/3.28/functions/@ember%2Fservice/inject).
+
+## Rule Details
+
+This rule checks for a configured list of previously auto injected services and warns if they are used in classes without explicit injected service properties.
+
+## Examples
+
+Examples of **incorrect** code for this rule:
+
+```js
+// routes/index.js
+import Route from '@ember/routing/route';
+
+export default class IndexRoute extends Route {
+  async model() {
+    return this.store.findAll('rental');
+  }
+}
+
+```
+
+```js
+// controller/index.js
+import Route from '@ember/routing/route';
+import { action } from '@ember/object';
+
+export default class IndexController extends Controller {
+  @action
+  async loadUsers() {
+    return this.store.findAll('user');
+  }
+}
+```
+
+Examples of **correct** code for this rule:
+
+```js
+// routes/index.js
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+
+export default class IndexRoute extends Route {
+  @service('store') store;
+
+  async model() {
+    return this.store.findAll('rental');
+  }
+}
+```
+
+```js
+// controller/index.js
+import Route from '@ember/routing/route';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class IndexController extends Controller {
+  @service('store') store;
+
+  @action
+  async loadUsers() {
+    return this.store.findAll('user');
+  }
+}
+```
+
+## Migration
+
+The autofixer for this rule will update classes and add injections for the configured services.
+
+## Configuration
+
+This lint rule will search for instances of `store` used in routes or controllers by default. If you have other services that you would like to check for uses of, the configuration can be overridden.
+
+- object -- containing the following properties:
+  - array -- `services` -- Array of configuration objects configuring the lint rule to check for use of implicit injected services
+    - string -- `serviceName` -- The name of the service that is implicitly injected
+    - array -- `moduleNames` -- Array of string listing the types of classes (controller, route, component, etc) to check for implicit injections. If an array is declared only those class types will be checked for implicit injection. (Defaults to checking all class files/types)
+
+Example config:
+
+```js
+module.exports = {
+  rules: {
+    'ember/no-implicit-injections': {
+      services: [
+        // Ember Responsive Used to Auto Inject the media service in Components/Controllers
+        { serviceName: 'media', moduleNames: ['Component', 'Controller'] },
+        // Ember CLI Flash Used to Auto Inject the flashMessages service in all modules
+        { serviceName: 'flashMessages' },
+      ]
+    }
+  }
+}
+```
+
+## Related Rules
+
+- [no-unnecessary-service-injection-argument](https://github.com/ember-cli/eslint-plugin-ember/blob/master/docs/rules/no-unnecessary-service-injection-argument.md) omit service injection argument if possible
+- [no-implicit-service-injection-argument](https://github.com/ember-cli/eslint-plugin-ember/blob/master/docs/rules/no-unnecessary-service-injection-argument.md) require the service injection argument for all services (fix output for this rule follows this pattern)
+
+## References
+
+- [Deprecation](https://deprecations.emberjs.com/v3.x/#toc_implicit-injections)
---
+++ tests/lib/rules/no-implicit-injections.js
@@ -0,0 +1,228 @@
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/no-implicit-injections');
+const RuleTester = require('eslint').RuleTester;
+
+const { ERROR_MESSAGE } = rule;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parser: require.resolve('@babel/eslint-parser'),
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    babelOptions: {
+      configFile: require.resolve('../../../.babelrc'),
+    },
+  },
+});
+
+ruleTester.run('no-implicit-injections', rule, {
+  valid: [
+    {
+      filename: 'routes/index.js',
+      code: `
+      import Route from '@ember/routing/route';
+      import { inject as service } from '@ember/service';
+
+      export default class IndexRoute extends Route {
+        @service('store') store;
+        async model() {
+          return this.store.findAll('rental');
+        }
+      }`,
+    },
+    {
+      filename: 'controller/index.js',
+      code: `
+      import Controller from '@ember/controller';
+      import { action } from '@ember/object';
+      import { inject as service } from '@ember/service';
+
+      export default class IndexController extends Controller {
+        @service('store') store;
+        @action
+        async loadUsers() {
+          return this.store.findAll('user');
+        }
+      }`,
+    },
+    // Only checks for ThisExpression
+    {
+      filename: 'controller/index.js',
+      code: `
+      import Controller from '@ember/controller';
+      import { action } from '@ember/object';
+
+      export default class IndexController extends Controller {
+        @action
+        async loadUsers(arg) {
+          return arg.store.findAll('user');
+        }
+      }`,
+    },
+    {
+      filename: 'components/foobar.js',
+      code: `
+      import Component from '@ember/component';
+
+      export default class FoobarTest extends Component {
+        async model() {
+          return this.store.isXs;
+        }
+      }`,
+    },
+    {
+      filename: 'components/foobar.js',
+      code: `
+      import Component from '@glimmer/component';
+
+      export default class FoobarTest extends Component {
+        async model() {
+          return this.store.isXs;
+        }
+      }`,
+    },
+    {
+      filename: 'routes/index.js',
+      code: `
+      import Route from '@ember/routing/route';
+
+      export default class IndexRoute extends Route {
+        get isSmallScreen() {
+          return this.media.isXs;
+        }
+      }`,
+      options: [
+        {
+          services: [{ serviceName: 'media', moduleNames: ['Component', 'Controller'] }],
+        },
+      ],
+    },
+    {
+      filename: 'utils/support.js',
+      code: `
+      export function checkMedia() {
+        return this.media.isXs;
+      }`,
+      options: [
+        {
+          services: [{ serviceName: 'media' }],
+        },
+      ],
+    },
+  ],
+  invalid: [
+    {
+      filename: 'routes/index.js',
+      code: `
+      import Route from '@ember/routing/route';
+
+      export default class IndexRoute extends Route {
+        message = 'hello';
+        async model() {
+          return this.store.findAll('rental');
+        }
+      }`,
+      output: `
+      import { inject as service } from '@ember/service';
+import Route from '@ember/routing/route';
+
+      export default class IndexRoute extends Route {
+        @service('store') store;
+message = 'hello';
+        async model() {
+          return this.store.findAll('rental');
+        }
+      }`,
+      errors: [{ message: ERROR_MESSAGE, type: 'MemberExpression' }],
+    },
+    {
+      filename: 'controller/index.js',
+      code: `
+      import Controller from '@ember/controller';
+      import { action } from '@ember/object';
+
+      export default class IndexController extends Controller {
+        @action
+        async loadUsers() {
+          return this.store.findAll('user');
+        }
+      }`,
+      output: `
+      import { inject as service } from '@ember/service';
+import Controller from '@ember/controller';
+      import { action } from '@ember/object';
+
+      export default class IndexController extends Controller {
+        @service('store') store;
+@action
+        async loadUsers() {
+          return this.store.findAll('user');
+        }
+      }`,
+      errors: [{ message: ERROR_MESSAGE, type: 'MemberExpression' }],
+    },
+    {
+      filename: 'components/foobar.js',
+      code: `
+      import Component from '@ember/component';
+
+      export default class FoobarTestError extends Component {
+        get isSmallScreen() {
+          return this.media.isXs;
+        }
+      }`,
+      output: `
+      import { inject as service } from '@ember/service';
+import Component from '@ember/component';
+
+      export default class FoobarTestError extends Component {
+        @service('media') media;
+get isSmallScreen() {
+          return this.media.isXs;
+        }
+      }`,
+      options: [
+        {
+          services: [{ serviceName: 'media', moduleNames: ['Component', 'Controller'] }],
+        },
+      ],
+      errors: [{ message: ERROR_MESSAGE, type: 'MemberExpression' }],
+    },
+    {
+      filename: 'components/foobar.js',
+      code: `
+      import Component from '@ember/component';
+
+      export default class FoobarTestError extends Component {
+        @action
+        get save() {
+          return this.flashMessages.warn('some message');
+        }
+      }`,
+      output: `
+      import { inject as service } from '@ember/service';
+import Component from '@ember/component';
+
+      export default class FoobarTestError extends Component {
+        @service('flash-messages') flashMessages;
+@action
+        get save() {
+          return this.flashMessages.warn('some message');
+        }
+      }`,
+      options: [
+        {
+          services: [{ serviceName: 'flashMessages' }],
+        },
+      ],
+      errors: [{ message: ERROR_MESSAGE, type: 'MemberExpression' }],
+    },
+  ],
+});
