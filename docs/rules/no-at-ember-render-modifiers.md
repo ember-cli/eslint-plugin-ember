779dc01a6811b03c4dd01725bbbf4f04004ded4c
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-06-30 21:12:16 -0400

    Add tests, run update commands

---
+++ docs/rules/no-at-ember-render-modifiers.md
@@ -0,0 +1,7 @@
+# ember/no-at-ember-render-modifiers
+
+<!-- end auto-generated rule header -->
+
+## Examples
+
+## References
---
+++ tests/lib/rules/no-at-ember-render-modifiers.js
@@ -0,0 +1,54 @@
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/no-at-ember-render-modifiers');
+const RuleTester = require('eslint').RuleTester;
+
+const { ERROR_MESSAGE } = rule;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+});
+ruleTester.run('no-at-ember-render-modifiers', rule, {
+  valid: ['import x from "x"', ''],
+  invalid: [
+    {
+      code: 'import "@ember/render-modifiers";',
+      output: null,
+      errors: [{ message: ERROR_MESSAGE, type: 'ImportDeclaration' }],
+    },
+    {
+      code: 'import x from "@ember/render-modifiers";',
+      output: null,
+      errors: [{ message: ERROR_MESSAGE, type: 'ImportDeclaration' }],
+    },
+    {
+      code: 'import { x } from "@ember/render-modifiers";',
+      output: null,
+      errors: [{ message: ERROR_MESSAGE, type: 'ImportDeclaration' }],
+    },
+    {
+      code: 'import didInsert from "@ember/render-modifiers/modifiers/did-insert";',
+      output: null,
+      errors: [{ message: ERROR_MESSAGE, type: 'ImportDeclaration' }],
+    },
+    {
+      code: 'import didUpdate from "@ember/render-modifiers/modifiers/did-update";',
+      output: null,
+      errors: [{ message: ERROR_MESSAGE, type: 'ImportDeclaration' }],
+    },
+    {
+      code: 'import willDestroy from "@ember/render-modifiers/modifiers/will-destroy";',
+      output: null,
+      errors: [{ message: ERROR_MESSAGE, type: 'ImportDeclaration' }],
+    },
+  ],
+});
