372f6b89712afa0648475ea66cccd0f6fe411c32
Patrick Pircher <patricklx@users.noreply.github.com>
2023-11-03 19:18:59 +0100

    Add new rule `template-no-let-reference` (#1939)
    Co-authored-by: Bryan Mishkin <698306+bmish@users.noreply.github.com>

---
+++ tests/lib/rules/template-no-let-reference.js
@@ -0,0 +1,62 @@
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/template-no-let-reference');
+const RuleTester = require('eslint').RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+  parser: require.resolve('../../../lib/parsers/gjs-gts-parser.js'),
+  parserOptions: { ecmaVersion: 2020, sourceType: 'module' },
+});
+ruleTester.run('template-no-let-reference', rule, {
+  valid: [
+    `
+      const a = '';
+      <template>
+      {{a}}
+      </template>
+    `,
+    `
+      const a = '';
+      <template>
+      <a></a>
+      </template>
+    `,
+    // make sure rule does not error out on missing references
+    `
+      const a = '';
+      <template>
+      {{ab}}
+      <ab></ab>
+      </template>
+    `,
+  ],
+
+  invalid: [
+    {
+      code: `
+      let a = '';
+      <template>
+      {{a}}
+      </template>
+      `,
+      output: null,
+      errors: [{ type: 'VarHead', message: rule.meta.messages['no-let'] }],
+    },
+    {
+      code: `
+      var a = '';
+      <template>
+      <a></a>
+      </template>
+      `,
+      output: null,
+      errors: [{ type: 'GlimmerElementNode', message: rule.meta.messages['no-let'] }],
+    },
+  ],
+});
