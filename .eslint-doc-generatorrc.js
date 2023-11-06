d84ed107d4e2391cf021c933824dc0154e6a03cb
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2023-11-06 11:14:20 -0500

    Update .eslint-doc-generatorrc.js
    alphabetical

--- .eslint-doc-generatorrc.js
+++ .eslint-doc-generatorrc.js
@@ -1,11 +1,11 @@
 /** @type {import('eslint-doc-generator').GenerateOptions} */
 module.exports = {
-  ruleDocSectionInclude: ['Examples'],
-  ruleDocTitleFormat: 'prefix-name',
-  ruleListSplit: 'meta.docs.category',
-  urlConfigs: 'https://github.com/ember-cli/eslint-plugin-ember#-configurations',
   configEmoji: [
     ["recommended-gjs", "![gjs logo](/docs/svgs/gjs.svg)"],
     ["recommended-gts", "![gts logo](/docs/svgs/gts.svg)"]
   ],
+  ruleDocSectionInclude: ['Examples'],
+  ruleDocTitleFormat: 'prefix-name',
+  ruleListSplit: 'meta.docs.category',
+  urlConfigs: 'https://github.com/ember-cli/eslint-plugin-ember#-configurations',
 };
