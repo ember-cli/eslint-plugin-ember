dd8d880f1f8017d6abb33ae4ad8ddb340fa54fe3
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2023-12-12 18:48:52 -0500

    fix merge mistakes

--- tests/plugin-exports.js
+++ tests/plugin-exports.js
@@ -2,10 +2,10 @@
 
 const plugin = require('../lib');
 const ember = require('../lib/utils/ember');
-const base = require('../lib/config/base');
+const base = require('../lib/config-legacy/base');
 const recommended = require('../lib/config-legacy/recommended');
-const recommendedGjs = require('../lib/config/recommended-gjs');
-const recommendedGts = require('../lib/config/recommended-gts');
+const recommendedGjs = require('../lib/config-legacy/recommended-gjs');
+const recommendedGts = require('../lib/config-legacy/recommended-gts');
 
 describe('plugin exports', () => {
   describe('utils', () => {
