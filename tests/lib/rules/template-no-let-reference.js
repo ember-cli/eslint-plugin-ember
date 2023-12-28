a65cd07dd7f0fffa3fcdc05121567dd3cd1469b5
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-12-28 15:21:21 -0500

    fix template-no-let-reference

--- tests/lib/rules/template-no-let-reference.js
+++ tests/lib/rules/template-no-let-reference.js
@@ -43,6 +43,28 @@ ruleTester.run('template-no-let-reference', rule, {
       <ab></ab>
       </template>
     `,
+    `
+    import { ExternalLink } from 'limber-ui';
+
+    export const Footer = <template>
+      <footer class="p-2 w-full grid shadow border-t">
+        <nav class="mx-auto flex gap-4">
+          <ExternalLink href="https://github.com/nullvoxpopuli/limber">
+            <:custom as |DefaultContent|>
+              <span class="hidden sm:block">
+                <DefaultContent>
+                  GitHub
+                </DefaultContent>
+              </span>
+            </:custom>
+          </ExternalLink>
+          <ExternalLink href="https://guides.emberjs.com/">
+            Guides
+          </ExternalLink>
+        </nav>
+      </footer>
+    </template>;
+    `
   ],
 
   invalid: [
