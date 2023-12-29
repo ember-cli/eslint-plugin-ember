d67cc1a3503585bcd6f27191746646185ad41205
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-12-29 14:06:37 -0500

    Failing tets for no deprecatde router transition methods

--- tests/lib/rules/no-deprecated-router-transition-methods.js
+++ tests/lib/rules/no-deprecated-router-transition-methods.js
@@ -358,6 +358,116 @@ ruleTester.run('no-deprecated-router-transition-methods', rule, {
         @computed.reads('model.actors') actors;
       }`,
     },
+
+    // Does not error when `this` is from a test context
+    {
+      filename: 'tests/application/output-demos-test.ts',
+      code: `
+      import Controller from '@ember/controller';
+      import { assert as debugAssert } from '@ember/debug';
+      import { settled, visit } from '@ember/test-helpers';
+      import { hbs } from 'ember-cli-htmlbars';
+      import { module, test } from 'qunit';
+      import { setupApplicationTest } from 'ember-qunit';
+
+      import { ALL, getFromLabel } from 'limber/snippets';
+      import { fileFromParams, type Format } from 'limber/utils/messaging';
+
+      import { getService } from '../helpers';
+      import { Page } from './-page';
+
+      module('Output > Demos', function (hooks) {
+        setupApplicationTest(hooks);
+
+        let page = new Page();
+
+        /**
+         * The editor is excluded from these tests,
+         * but in order for the URI to change, the editor must be "set up".
+         *
+         * The editor service knows when this happens via _editorSwapText
+         * being set. Until that property is set, calls to updateDemo will
+         * be ignored.
+         */
+        hooks.beforeEach(function () {
+          this.owner.lookup('service:editor')._editorSwapText = () => {};
+        });
+
+        module('every option correctly changes the query params', function () {
+          for (let demo of ALL) {
+            test(demo.label, async function (assert) {
+              this.owner.register('template:edit', hbs\`<Limber::DemoSelect />\`);
+
+              await visit('/edit');
+              await page.selectDemo(demo.label);
+
+              let { queryParams = {} } = getService('router').currentRoute ?? {};
+
+              let file = fileFromParams(queryParams);
+
+              assert.strictEqual(queryParams.format, 'glimdown');
+              assert.strictEqual(file.format, queryParams.format, 'format matches queryParams');
+              assert.strictEqual(queryParams.t, undefined, 'old format is no longer in use');
+              assert.notStrictEqual(queryParams.c, undefined, 'new format is is what is used');
+              assert.strictEqual(file.text, await getFromLabel(demo.label), 'detected text matches demo');
+            });
+          }
+        });
+
+        module('The output frame renders every demo', function () {
+          for (let demo of ALL) {
+            test(demo.label, async function (assert) {
+              let makeComponent!: (format: Format, text: string) => void;
+              let setParentFrame!: (parentAPI: {
+                beginCompile: () => void;
+                error: () => void;
+                success: () => void;
+                finishedRendering: () => void;
+              }) => void;
+
+              class FakeController extends Controller {
+                api = {
+                  onReceiveText: (callback: typeof makeComponent) => (makeComponent = callback),
+                  onConnect: (callback: typeof setParentFrame) => (setParentFrame = callback),
+                };
+              }
+              this.owner.register('controller:edit', FakeController);
+              this.owner.register(
+                'template:edit',
+                hbs\`
+                  <fieldset class="border">
+                    <legend>Limber::Output</legend>
+                    {{! @glint-ignore }}
+                    <Limber::Output @messagingAPI={{this.api}} />
+                  </fieldset>
+                  \`
+              );
+
+              await visit('/edit');
+
+              debugAssert(\`setParentFrame did not get set\`, setParentFrame);
+              debugAssert(\`makeComponent did not get set\`, makeComponent);
+
+              setParentFrame({
+                beginCompile: () => assert.step('begin compile'),
+                error: () => assert.step('error'),
+                success: () => assert.step('success'),
+                finishedRendering: () => assert.step('finished rendering'),
+              });
+
+              let text = await getFromLabel(demo.label);
+
+              makeComponent('glimdown', text);
+              await settled();
+
+              assert.verifySteps(['begin compile', 'success', 'finished rendering']);
+            });
+          }
+        });
+      });
+
+      `,
+    }
   ],
   invalid: [
     // Route Uses RouterService.transitionTo with different service injection types
