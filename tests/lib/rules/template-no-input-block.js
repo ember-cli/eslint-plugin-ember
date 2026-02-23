f972ca0c4c146114704232a8ed205b7cf328d61e
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-02-23 15:50:49 -0500

    Extract rule: template-no-input-block

---
+++ tests/lib/rules/template-no-input-block.js
@@ -0,0 +1,23 @@
+const rule = require('../../../lib/rules/template-no-input-block');
+const RuleTester = require('eslint').RuleTester;
+
+const ruleTester = new RuleTester({
+  parser: require.resolve('ember-eslint-parser'),
+  parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
+});
+ruleTester.run('template-no-input-block', rule, {
+  valid: [
+    '<template>{{input value=this.foo}}</template>',
+    // Test cases ported from ember-template-lint
+    '<template>{{button}}</template>',
+    '<template>{{#x-button}}{{/x-button}}</template>',
+    '<template>{{input}}</template>',
+  ],
+  invalid: [
+    {
+      code: '<template>{{#input}}{{/input}}</template>',
+      output: null,
+      errors: [{ messageId: 'blockUsage' }],
+    },
+  ],
+});
