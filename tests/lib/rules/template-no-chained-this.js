4f0db93fe5c793398273e1fac9b38d2f55c2e4c6
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-02-24 17:14:24 -0500

    Fix pairity with template-lint

--- tests/lib/rules/template-no-chained-this.js
+++ tests/lib/rules/template-no-chained-this.js
@@ -34,14 +34,12 @@ ruleTester.run('template-no-chained-this', rule, {
     },
     {
       code: '<template><this.this.Component /></template>',
-      output: null,
+      output: '<template><this.Component /></template>',
       errors: [{ messageId: 'noChainedThis' }],
     },
-
-    // Test cases ported from ember-template-lint
     {
       code: '<template>{{#this.this.value}}woo{{/this.this.value}}</template>',
-      output: null,
+      output: '<template>{{#this.value}}woo{{/this.value}}</template>',
       errors: [{ messageId: 'noChainedThis' }],
     },
     {
