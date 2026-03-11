97911c9436a3d84bb8710a1f5d8e5fcf305dd702
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-03-11 10:54:06 -0400

    Extract rule: template-no-html-comments

---
+++ docs/rules/template-no-html-comments.md
@@ -0,0 +1,41 @@
+# ember/template-no-html-comments
+
+🔧 This rule is automatically fixable by the [`--fix` CLI option](https://eslint.org/docs/latest/user-guide/command-line-interface#--fix).
+
+<!-- end auto-generated rule header -->
+
+Disallow HTML comments in templates. HTML comments will be visible in the rendered output, which may expose sensitive information or clutter the DOM.
+
+## Rule Details
+
+This rule disallows HTML comments (`<!-- -->`) in templates and suggests using Glimmer comments (`{{! }}` or `{{!-- --}}`) instead.
+
+## Examples
+
+Examples of **incorrect** code for this rule:
+
+```gjs
+<template>
+  <!-- This is an HTML comment -->
+  <div>Content</div>
+</template>
+```
+
+Examples of **correct** code for this rule:
+
+```gjs
+<template>
+  {{! This is a Glimmer comment }}
+  <div>Content</div>
+</template>
+
+<template>
+  {{!-- This is a block Glimmer comment --}}
+  <div>Content</div>
+</template>
+```
+
+## References
+
+- [Ember guides/template features](https://guides.emberjs.com/release/components/#toc_supported-features)
+- [Handlebars docs/comments](https://handlebarsjs.com/guide/#template-comments)
---
+++ tests/lib/rules/template-no-html-comments.js
@@ -0,0 +1,74 @@
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require('../../../lib/rules/template-no-html-comments');
+const RuleTester = require('eslint').RuleTester;
+
+const ruleTester = new RuleTester({
+  parser: require.resolve('ember-eslint-parser'),
+  parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
+});
+
+ruleTester.run('template-no-html-comments', rule, {
+  valid: [
+    '<template>{{! This is a comment }}</template>',
+    '<template>{{!-- This is a comment --}}</template>',
+    '<template><div>Content</div></template>',
+
+    '<template>{{!-- comment here --}}</template>',
+    '<template>{{!--comment here--}}</template>',
+  ],
+  invalid: [
+    {
+      code: '<template><!-- HTML comment --></template>',
+      output: '<template>{{! HTML comment }}</template>',
+      errors: [{ messageId: 'noHtmlComments' }],
+    },
+
+    {
+      code: '<template><!-- comment here --></template>',
+      output: '<template>{{! comment here }}</template>',
+      errors: [{ messageId: 'noHtmlComments' }],
+    },
+    {
+      code: '<template><!--comment here--></template>',
+      output: '<template>{{!comment here}}</template>',
+      errors: [{ messageId: 'noHtmlComments' }],
+    },
+    {
+      code: '<template><!--\n    line one\n    line two\n  --></template>',
+      output: '<template>{{!\n    line one\n    line two\n  }}</template>',
+      errors: [{ messageId: 'noHtmlComments' }],
+    },
+  ],
+});
+
+const hbsRuleTester = new RuleTester({
+  parser: require.resolve('ember-eslint-parser/hbs'),
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+});
+
+hbsRuleTester.run('template-no-html-comments', rule, {
+  valid: [
+    '{{!-- comment here --}}',
+    '{{!--comment here--}}',
+    '{{! template-lint-disable no-bare-strings }}',
+    '{{! template-lint-disable }}',
+  ],
+  invalid: [
+    {
+      code: '<!-- comment here -->',
+      output: '{{! comment here }}',
+      errors: [{ messageId: 'noHtmlComments' }],
+    },
+    {
+      code: '<!--comment here-->',
+      output: '{{!comment here}}',
+      errors: [{ messageId: 'noHtmlComments' }],
+    },
+  ],
+});
