2ce5b64d71e1d414dbea7cdad2b5a8439fd091e6
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-03-20 14:41:09 -0400

    Sync with template-lint

--- docs/rules/template-require-each-key.md
+++ docs/rules/template-require-each-key.md
@@ -1,36 +1,96 @@
 # ember/template-require-each-key
 
+🔧 This rule is automatically fixable by the [`--fix` CLI option](https://eslint.org/docs/latest/user-guide/command-line-interface#--fix).
+
 <!-- end auto-generated rule header -->
 
-Require a `key` attribute in `{{#each}}` loops for better rendering performance and to avoid rendering issues.
+In order to improve rendering speed, Ember will try to reuse the DOM elements where possible. Specifically, if the same item is present in the array both before and after the change, its DOM output will be reused.
 
-## Rule Details
+The key option is used to tell Ember how to determine if the items in the array being iterated over with `{{#each}}` has changed between renders. By default the item's object identity is used.
 
-This rule enforces using a `key` attribute in `{{#each}}` blocks to help Ember track items efficiently.
+This is usually sufficient, so in most cases, the key option is simply not needed. However, in some rare cases, the objects' identities may change even though they represent the same underlying data.
 
-## Examples
+For example:
 
-Examples of **incorrect** code for this rule:
+```js
+people.map((person) => {
+  return { ...person, type: 'developer' };
+});
+```
+
+In this case, each time the people array is map-ed over, it will produce an new array with completely different objects between renders. In these cases, you can help Ember determine how these objects related to each other with the key option:
 
 ```gjs
 <template>
-  {{#each items as |item|}}
+  <ul>
+    {{#each @developers key='name' as |person|}}
+      <li>Hello, {{person.name}}!</li>
+    {{/each}}
+  </ul>
+</template>
+```
+
+By doing so, Ember will use the value of the property specified (`person.name` in the example) to find a "match" from the previous render. That is, if Ember has previously seen an object from the `@developers` array with a matching name, its DOM elements will be re-used.
+
+This rule will require to always use `key` with `{{#each}}`.
+
+## Examples
+
+This rule **forbids** the following:
+
+```gjs
+<template>
+  {{#each this.items as |item|}}
     <div>{{item.name}}</div>
   {{/each}}
 </template>
 ```
 
-Examples of **correct** code for this rule:
-
 ```gjs
 <template>
-  {{#each items key="id" as |item|}}
+  {{#each this.items key='@invalid' as |item|}}
     <div>{{item.name}}</div>
   {{/each}}
 </template>
+```
 
+```gjs
 <template>
-  {{#each items key="@index" as |item|}}
+  {{#each this.items key='' as |item|}}
+    <div>{{item.name}}</div>
+  {{/each}}
+</template>
+```
+
+This rule **allows** the following:
+
+```gjs
+<template>
+  {{#each this.items key='id' as |item|}}
+    <div>{{item.name}}</div>
+  {{/each}}
+</template>
+```
+
+```gjs
+<template>
+  {{#each this.items key='deeply.nested.id' as |item|}}
+    <div>{{item.name}}</div>
+  {{/each}}
+</template>
+```
+
+```gjs
+<template>
+  {{#each this.items key='@index' as |item|}}
+    <div>{{item.name}}</div>
+  {{/each}}
+</template>
+```
+
+```gjs
+<template>
+  {{#each this.items key='@identity' as |item|}}
     <div>{{item.name}}</div>
   {{/each}}
 </template>
@@ -39,3 +99,4 @@ Examples of **correct** code for this rule:
 ## References
 
 - [Specifying Keys](https://api.emberjs.com/ember/release/classes/Ember.Templates.helpers/methods/each#specifying-keys)
+- [The Immutable Pattern in Tracked Properties](https://guides.emberjs.com/release/in-depth-topics/autotracking-in-depth/)
--- tests/lib/rules/template-require-each-key.js
+++ tests/lib/rules/template-require-each-key.js
@@ -1,50 +1,54 @@
-//------------------------------------------------------------------------------
-// Requirements
-//------------------------------------------------------------------------------
-
 const rule = require('../../../lib/rules/template-require-each-key');
 const RuleTester = require('eslint').RuleTester;
 
-const ruleTester = new RuleTester({
+const ERROR_MESSAGE = '{{#each}} helper requires a valid key value to avoid performance issues';
+
+const validHbs = [
+  '{{#each this.items key="id" as |item|}} {{item.name}} {{/each}}',
+  '{{#each this.items key="deeply.nested.id" as |item|}} {{item.name}} {{/each}}',
+  '{{#each this.items key="@index" as |item|}} {{item.name}} {{/each}}',
+  '{{#each this.items key="@identity" as |item|}} {{item.name}} {{/each}}',
+  '{{#if foo}}{{/if}}',
+];
+
+const invalidHbs = [
+  {
+    code: '{{#each this.items as |item|}} {{item.name}} {{/each}}',
+    output: '{{#each this.items key="@identity" as |item|}} {{item.name}} {{/each}}',
+    errors: [{ message: ERROR_MESSAGE }],
+  },
+  {
+    code: '{{#each this.items key="@invalid" as |item|}} {{item.name}} {{/each}}',
+    output: '{{#each this.items key="@identity" as |item|}} {{item.name}} {{/each}}',
+    errors: [{ message: ERROR_MESSAGE }],
+  },
+  {
+    code: '{{#each this.items key="" as |item|}} {{item.name}} {{/each}}',
+    output: '{{#each this.items key="@identity" as |item|}} {{item.name}} {{/each}}',
+    errors: [{ message: ERROR_MESSAGE }],
+  },
+];
+
+function wrapTemplate(entry) {
+  if (typeof entry === 'string') {
+    return `<template>${entry}</template>`;
+  }
+
+  return {
+    ...entry,
+    code: `<template>${entry.code}</template>`,
+    output: entry.output ? `<template>${entry.output}</template>` : entry.output,
+  };
+}
+
+const gjsRuleTester = new RuleTester({
   parser: require.resolve('ember-eslint-parser'),
   parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
 });
 
-ruleTester.run('template-require-each-key', rule, {
-  valid: [
-    '<template>{{#each items key="id" as |item|}}{{item.name}}{{/each}}</template>',
-    '<template>{{#each items key="@index" as |item|}}{{item.name}}{{/each}}</template>',
-    '<template><div>No each block</div></template>',
-
-    '<template>{{#each this.items key="id" as |item|}} {{item.name}} {{/each}}</template>',
-    '<template>{{#each this.items key="deeply.nested.id" as |item|}} {{item.name}} {{/each}}</template>',
-    '<template>{{#each this.items key="@index" as |item|}} {{item.name}} {{/each}}</template>',
-    '<template>{{#each this.items key="@identity" as |item|}} {{item.name}} {{/each}}</template>',
-    '<template>{{#if foo}}{{/if}}</template>',
-  ],
-  invalid: [
-    {
-      code: '<template>{{#each items as |item|}}{{item.name}}{{/each}}</template>',
-      output: null,
-      errors: [{ messageId: 'requireEachKey' }],
-    },
-
-    {
-      code: '<template>{{#each this.items as |item|}} {{item.name}} {{/each}}</template>',
-      output: null,
-      errors: [{ messageId: 'requireEachKey' }],
-    },
-    {
-      code: '<template>{{#each this.items key="@invalid" as |item|}} {{item.name}} {{/each}}</template>',
-      output: null,
-      errors: [{ messageId: 'requireEachKey' }],
-    },
-    {
-      code: '<template>{{#each this.items key="" as |item|}} {{item.name}} {{/each}}</template>',
-      output: null,
-      errors: [{ messageId: 'requireEachKey' }],
-    },
-  ],
+gjsRuleTester.run('template-require-each-key', rule, {
+  valid: validHbs.map(wrapTemplate),
+  invalid: invalidHbs.map(wrapTemplate),
 });
 
 const hbsRuleTester = new RuleTester({
@@ -56,34 +60,6 @@ const hbsRuleTester = new RuleTester({
 });
 
 hbsRuleTester.run('template-require-each-key', rule, {
-  valid: [
-    '{{#each this.items key="id" as |item|}} {{item.name}} {{/each}}',
-    '{{#each this.items key="deeply.nested.id" as |item|}} {{item.name}} {{/each}}',
-    '{{#each this.items key="@index" as |item|}} {{item.name}} {{/each}}',
-    '{{#each this.items key="@identity" as |item|}} {{item.name}} {{/each}}',
-    '{{#if foo}}{{/if}}',
-  ],
-  invalid: [
-    {
-      code: '{{#each this.items as |item|}} {{item.name}} {{/each}}',
-      output: null,
-      errors: [
-        { message: 'each block should have a key attribute for better rendering performance.' },
-      ],
-    },
-    {
-      code: '{{#each this.items key="@invalid" as |item|}} {{item.name}} {{/each}}',
-      output: null,
-      errors: [
-        { message: 'each block should have a key attribute for better rendering performance.' },
-      ],
-    },
-    {
-      code: '{{#each this.items key="" as |item|}} {{item.name}} {{/each}}',
-      output: null,
-      errors: [
-        { message: 'each block should have a key attribute for better rendering performance.' },
-      ],
-    },
-  ],
+  valid: validHbs,
+  invalid: invalidHbs,
 });
